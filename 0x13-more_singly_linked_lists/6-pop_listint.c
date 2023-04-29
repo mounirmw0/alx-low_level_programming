cf1189cb9eda286f24279fe90dc16210da68e98c
mounirmw0 <mounirmw0@gmail.com>
2023-04-29 16:51:43 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - pops a node out
+ * @head: linked list address
+ * Return: head node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
